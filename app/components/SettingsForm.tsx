21fedc7cda7a150158f692fd969d8ce809e780cb
Ashish Kumar <ashishkumar901336@gmail.com>
2025-04-08 21:35:09 +0530

    starting settings page

---
+++ app/components/SettingsForm.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export function SettingsForm() {
+    return (
+        <form>
+            <h1 className="text-3xl font-extrabold tracking-tight">Settings</h1>
+        </form>
+    )
+}
